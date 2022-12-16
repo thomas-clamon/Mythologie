c9ca94497ec4e71212cf4cb147ab3e1482896173
thomas clamon <thomas.clamon@gmail.com>
2022-12-16 11:20:15 +0100

    first commit

---
+++ src/test/java/com/example/mythologie/MythologieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mythologie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MythologieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
